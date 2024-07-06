c20b6e82ee1bf0bd1358797b8be6bd430e1dec76
Alireza Safaei <alirezasafaei9494@gmail.com>
2024-07-07 01:03:00 +0330

    add formdata struct

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,23 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-// Notice the different signature!
-// We return `Server` on the happy path and we dropped the `async` keyword
-// We have no .await call, so it is not needed anymore.
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
-    // No .await here!
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
